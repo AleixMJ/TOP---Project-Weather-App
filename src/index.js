b4396c5393da17bda28f59a2c838b4bcd70e48a7
AleixMJ <aleixtechIT@gmail.com>
2026-05-13 15:43:43 +0200

    Added fetch functionality to get weather data
    The fetch is done via visualcrossing API, which is a paid API but has a
    free tier that allows for 1000 requests per day. For now the Weather
    output is just logged to the console, but in the future it will be
    displayed on the page.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+
+const form = document.getElementById("weatherForm");
+form.addEventListener('submit', checkWeather);
+
+async function checkWeather(event) {
+    event.preventDefault();
+    console.log("event starting")
+    let city = document.getElementById("city").value.trim();
+    console.log(city);
+    let cityWeather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=DD5CVPTKTMU25CGMKJMCLRSXG`);
+    console.log(cityWeather);
+}
+
+
